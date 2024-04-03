d98b4f729aded02a5be159e10b065edd41fdf64b
Tinsae Mekbib <tinsae.mekbib@tobania.be>
2024-04-04 02:17:14 +0300

    broken authentication code ....

---
+++ src/main/java/com/daddys/coffee/entities/AuthRequest.java
@@ -0,0 +1,15 @@
+package com.daddys.coffee.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthRequest {
+
+    private String username;
+    private String password;
+
+}
---
+++ src/main/java/com/daddys/coffee/repositories/UserInfoRepository.java
@@ -0,0 +1,13 @@
+package com.daddys.coffee.repositories;
+
+
+import com.daddys.coffee.entities.UserInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserInfoRepository extends JpaRepository<UserInfo, Integer> {
+    Optional<UserInfo> findByName(String username);
+}
